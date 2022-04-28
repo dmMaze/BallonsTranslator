fc3bfa5f2d80e80362fe6212e064e6b54c36f2d0
dmMaze <beneathlimbo@gmail.com>
2022-04-28 18:02:49 +0800

    fix build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 nuitka --standalone --mingw64 --nofollow-imports --show-memory --show-progress ^
     --enable-plugin=pyqt5 --include-qt-plugins=sensible,styles ^
-        --follow-import-to=dl,utils,ui --include-plugin-directory=BallonTranslator/dl,BallonTranslator/ui,BallonTranslator/utils ^
+        --follow-import-to=dl,utils,ui --include-plugin-directory=ballontranslator/dl,ballontranslator/ui,ballontranslator/utils ^
             --windows-product-version=1.1.3.0 --windows-company-name=DUMMY_WINDOWS_COMPANY_NAME --windows-product-name=BallonTranslator ^
-                --output-dir=release ballontranslator 
+                --output-dir=release BallonTranslator 
