16a01b7c473470816569ed99e4860791149269c0
Sergey <bropines@gmail.com>
2022-08-24 19:49:18 +0500

    Add russian locales

---
+++ ballontranslator/data/translate/template.ts
@@ -0,0 +1,414 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>BottomBar</name>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="524"/>
+        <source>Enable/disable ocr</source>
+        <translation>启用/禁用OCR</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="528"/>
+        <source>Enable/disable translation</source>
+        <translation>启用/禁用机翻</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="532"/>
+        <source>translate page</source>
+        <translation>翻译本页</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="532"/>
+        <source>stop</source>
+        <translation>停止</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="532"/>
+        <source>translate current page</source>
+        <translation>翻译本页</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="532"/>
+        <source>stop translation</source>
+        <translation>停止翻译</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="541"/>
+        <source>Enable/disable paint mode</source>
+        <translation>启用/禁用画板</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="545"/>
+        <source>Enable/disable text edit mode</source>
+        <translation>启用/禁用文本编辑</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="551"/>
+        <source>Original image transparency: </source>
+        <translation>原图透明度: </translation>
+    </message>
+</context>
+<context>
+    <name>Canvas</name>
+    <message>
+        <location filename="../ui/canvas.py" line="383"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigPanel</name>
+    <message>
+        <location filename="../ui/configpanel.py" line="251"/>
+        <source>Text Detection</source>
+        <translation>文本检测</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="272"/>
+        <source>OCR</source>
+        <translation>OCR</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="253"/>
+        <source>Inpaint</source>
+        <translation>图像修复</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="280"/>
+        <source>Translator</source>
+        <translation>翻译器</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="268"/>
+        <source>Detector</source>
+        <translation>检测器</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="276"/>
+        <source>Inpainter</source>
+        <translation>修复工具</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="248"/>
+        <source>DL Module</source>
+        <translation>自动化模组</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="249"/>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="283"/>
+        <source>Startup</source>
+        <translation>启动</translation>
+    </message>
+    <message>
+        <location filename="../ui/configpanel.py" line="284"/>
+        <source>Reopen last project on startup</source>
+        <translation>启动时打开上次项目</translation>
+    </message>
+</context>
+<context>
+    <name>DLManager</name>
+    <message>
+        <location filename="../ui/dl_manager.py" line="647"/>
+        <source>Invalid</source>
+        <translation>不可用</translation>
+    </message>
+</context>
+<context>
+    <name>DrawingPanel</name>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="288"/>
+        <source>Mask Transparency</source>
+        <translation>掩膜透明度</translation>
+    </message>
+</context>
+<context>
+    <name>FontFormatPanel</name>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="287"/>
+        <source>Font Family</source>
+        <translation>字体</translation>
+    </message>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="291"/>
+        <source>Font Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="297"/>
+        <source>Change font color</source>
+        <translation>改变文字颜色</translation>
+    </message>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="314"/>
+        <source>Change stroke color</source>
+        <translation>改变文字轮廓颜色</translation>
+    </message>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="319"/>
+        <source>Stroke width: </source>
+        <translation>轮廓宽度: </translation>
+    </message>
+    <message>
+        <location filename="../ui/fontformatpanel.py" line="324"/>
+        <source>line spacing: </source>
+        <translation>行间距: </translation>
+    </message>
+</context>
+<context>
+    <name>ImgtransThread</name>
+    <message>
+        <location filename="../ui/dl_manager.py" line="327"/>
+        <source>Translation Failed.</source>
+        <translation>翻译失败.</translation>
+    </message>
+</context>
+<context>
+    <name>InpaintConfigPanel</name>
+    <message>
+        <location filename="../ui/dlconfig_parse_widgets.py" line="229"/>
+        <source>Let the program decide whether it is necessary to use the selected inpaint method.</source>
+        <translation>由程序决定是否调用修复方法</translation>
+    </message>
+</context>
+<context>
+    <name>InpaintPanel</name>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="70"/>
+        <source>Thickness</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="64"/>
+        <source>pen thickness </source>
+        <translation>画笔大小 </translation>
+    </message>
+</context>
+<context>
+    <name>InpaintThread</name>
+    <message>
+        <location filename="../ui/dl_manager.py" line="107"/>
+        <source>Inpainting Failed.</source>
+        <translation>修复失败.</translation>
+    </message>
+</context>
+<context>
+    <name>InpainterStatusButton</name>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="74"/>
+        <source>Inpainter: </source>
+        <translation>修复工具: </translation>
+    </message>
+</context>
+<context>
+    <name>LeftBar</name>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="143"/>
+        <source>Open Folder ...</source>
+        <translation>打开文件夹...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="148"/>
+        <source>Open Project ... *.json</source>
+        <translation>打开项目文件...*.json</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="152"/>
+        <source>Save Project</source>
+        <translation>保存项目</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="157"/>
+        <source>Export as Doc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="160"/>
+        <source>Import from Doc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="163"/>
+        <source>Open Recent</source>
+        <translation>打开最近</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.py" line="207"/>
+        <source>Failed to load project </source>
+        <translation>项目加载失败 </translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.py" line="451"/>
+        <source>unsaved</source>
+        <translation>未保存</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.py" line="451"/>
+        <source>saved</source>
+        <translation>已保存</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.py" line="465"/>
+        <source>Saving image...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ModuleThread</name>
+    <message>
+        <location filename="../ui/dl_manager.py" line="51"/>
+        <source>Failed to set </source>
+        <translation>无法设置 </translation>
+    </message>
+</context>
+<context>
+    <name>PenConfigPanel</name>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="107"/>
+        <source>alpha value</source>
+        <translation>alpha值</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="115"/>
+        <source>Color</source>
+        <translation>颜色</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="116"/>
+        <source>Alpha</source>
+        <translation>Alpha</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="124"/>
+        <source>Thickness</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="103"/>
+        <source>pen thickness </source>
+        <translation>画笔大小 </translation>
+    </message>
+</context>
+<context>
+    <name>ProgressMessageBox</name>
+    <message>
+        <location filename="../ui/stylewidgets.py" line="99"/>
+        <source>Detecting: </source>
+        <translation>检测: </translation>
+    </message>
+    <message>
+        <location filename="../ui/stylewidgets.py" line="100"/>
+        <source>OCR: </source>
+        <translation>OCR: </translation>
+    </message>
+    <message>
+        <location filename="../ui/stylewidgets.py" line="101"/>
+        <source>Inpainting: </source>
+        <translation>修复: </translation>
+    </message>
+    <message>
+        <location filename="../ui/stylewidgets.py" line="102"/>
+        <source>Translating: </source>
+        <translation>翻译: </translation>
+    </message>
+</context>
+<context>
+    <name>RectPanel</name>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="162"/>
+        <source>method 1</source>
+        <translation>方法1</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="162"/>
+        <source>method 2</source>
+        <translation>方法2</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="163"/>
+        <source>Auto</source>
+        <translation>自动</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="164"/>
+        <source>run inpainting automatically.</source>
+        <translation>自动运行修复函数.</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="166"/>
+        <source>Inpaint</source>
+        <translation>图像修复</translation>
+    </message>
+    <message>
+        <location filename="../ui/drawingpanel.py" line="168"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+<context>
+    <name>TranslateThread</name>
+    <message>
+        <location filename="../ui/dl_manager.py" line="167"/>
+        <source>The selected language is not supported by </source>
+        <translation>所选语言不被目标翻译器支持</translation>
+    </message>
+    <message>
+        <location filename="../ui/dl_manager.py" line="168"/>
+        <source>support list: </source>
+        <translation>支持语言列表: </translation>
+    </message>
+    <message>
+        <location filename="../ui/dl_manager.py" line="174"/>
+        <source>Failed to set translator </source>
+        <translation>翻译器设置失败 </translation>
+    </message>
+    <message>
+        <location filename="../ui/dl_manager.py" line="227"/>
+        <source> is required for </source>
+        <translation>是翻译器必填项</translation>
+    </message>
+    <message>
+        <location filename="../ui/dl_manager.py" line="225"/>
+        <source>Translation Failed.</source>
+        <translation>翻译失败.</translation>
+    </message>
+</context>
+<context>
+    <name>TranslatorConfigPanel</name>
+    <message>
+        <location filename="../ui/dlconfig_parse_widgets.py" line="196"/>
+        <source>Source </source>
+        <translation>源语言 </translation>
+    </message>
+    <message>
+        <location filename="../ui/dlconfig_parse_widgets.py" line="198"/>
+        <source>Target </source>
+        <translation>目标语言 </translation>
+    </message>
+</context>
+<context>
+    <name>TranslatorStatusButton</name>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="67"/>
+        <source>Translator: </source>
+        <translation>翻译器： </translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="67"/>
+        <source>Source: </source>
+        <translation>源语言: </translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindowbars.py" line="67"/>
+        <source>Target: </source>
+        <translation>目标语言: </translation>
+    </message>
+</context>
+</TS>
