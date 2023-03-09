ccc77254ece28d165b573b323f3961bff149aa0a
John <98853508+ROKOLYT@users.noreply.github.com>
2023-03-09 17:31:52 +0100

    Delete ballontranslator/ui/pagesources directory

--- ballontranslator/ui/pagesources/constants.py
+++
@@ -1,6 +0,0 @@
-import os.path as osp
-
-UI_PATH = osp.dirname(osp.abspath(__file__))
-PROGRAM_PATH = osp.dirname(UI_PATH)
-SOURCE_DOWNLOAD_PATH = osp.join(PROGRAM_PATH, "pagesources\projects")
-
--- ballontranslator/ui/pagesources/exceptions.py
+++
@@ -1,2 +0,0 @@
-class SourceNotImplemented(Exception):
-    pass
