8c5a977a50b685120c5a2cd58a7a5b5f4705860e
hyrulelinks <134026642+hyrulelinks@users.noreply.github.com>
2023-09-11 00:49:41 -0500

    Update build-macos-app.sh

--- build-macos-app.sh
+++ build-macos-app.sh
@@ -2,30 +2,28 @@
 if command -v pyenv >/dev/null 2>&1; then
   pyenv install '3.11.5'
 else
-  echo "pyenv未安装"
+  echo "pyenv is not installed"
 fi
 
 # Activate Python 3.11.5 in global environment
 pyenv global '3.11.5'
 
-# Create Python 3.11.5 virtual environment
+# Create and activate Python 3.11.5 virtual environment
 python -m venv 'venv'
-
-# Activate Python 3.11.5 virtual environment
 source 'venv/bin/activate'
 
-# install dependencies
+# Install dependencies
 pip install -r requirements.txt
 pip install pyinstaller
 
-# Copy 'libs' and 'models' directories to 'data'
+# Copy required directories to data
 ditto ../libs data/libs -V
 ditto ../models data/models -V
 
-# Comment line 213-229 of launch.py
+# Comment lines 213-229 of launch.py
 cp launch.py launch.py.bak # backup launch.py
 sed -i '' '213,229s|^| \#|' launch.py # comment specfied lines
 sed -n '213,229p' launch.py # check if comment is successful
 
-# Pack macOS standalone app via pyinstaller
+# Build macOS app via pyinstaller
 sudo pyinstaller launch.spec
