2110591b21a018d2ef1913e0ace76b626a33c073
dmMaze <beneathlimbo@gmail.com>
2024-07-29 13:01:45 +0800

    fix #524

--- launch.py
+++ launch.py
@@ -288,8 +288,7 @@ def prepare_environment():
             except Exception:
                 run_pip(f"install {req}", req)
                 req_updated = True
-
-    torch_command = os.environ.get('TORCH_COMMAND', "pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118 --disable-pip-version-check")
+    torch_command = os.environ.get('TORCH_COMMAND', "pip install torch==2.2.2 torchvision==0.17.2 --index-url https://download.pytorch.org/whl/cu118 --disable-pip-version-check")
     if args.reinstall_torch or not is_installed("torch") or not is_installed("torchvision"):
         run(f'"{python}" -m {torch_command}', "Installing torch and torchvision", "Couldn't install torch", live=True)
         req_updated = True
