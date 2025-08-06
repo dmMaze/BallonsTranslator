4db588aad7208b6c66d24053324b88d5ef9b0173
zzzzzzzskyward <zzzzzzzskyward@github.io>
2025-08-06 17:17:05 +0800

    move cache position

--- trans.jp.py
+++ trans.jp.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 from collections import deque
 from google.genai import types
 from google import genai
+import simplejson as json
 from pydantic import BaseModel
 import tqdm
 import sys
@@ -16,7 +17,7 @@ from api_key import google as API_KEY
 from api_key import deepseek as DEEPSEEK_API_KEY
 ################
 client = OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com")
-ai_service="deepseek"
+ai_service = "deepseek"
 sys_message = '''你是中国本土的专业日语翻译家，你能够精确、忠实、流畅地翻译日语为生活化、通俗易懂的简体中文。现在有一些待翻译的漫画文本（存在OCR识别错误的可能），请结合语境给出一句完整的翻译。直接回答翻译语句，不做任何解释，不添加引号。对于无法翻译的句子，直接返回原文。
 '''
 '''
@@ -30,7 +31,7 @@ sys_message = '''你是中国本土的专业日语翻译家，你能够精确、
 first = ''  # '''历史记录：'MAS PRAM!!'='普拉姆先生！！''''
 id = 0
 cache = []
-
+cache_name=""
 
 def waitfor():
     time.sleep(10)
@@ -38,17 +39,43 @@ def waitfor():
 
 def read_cache():
     global cache
-    if os.path.exists("./cache.txt"):
-        with open("./cache.txt", encoding='utf-8') as f:
-            cache = [line.strip() for line in f]
+    if os.path.exists(f"cache/{cache_name}.json"):
+        with open("./cache.json", encoding='utf-8') as f:
+            cache = json.load(f)
+
+
+def read_from_previous(data):
+    if not data:
+        return
+    lines = data.split('\n\n')
+    validlines = [i.strip() for i in lines]
+    v = []
+    for i in validlines:
+        if i.startswith('#'):
+            continue
+        dot = i.find('.')
+        if dot > 0:
+            dot_prev = i[:dot].strip()
+            dot_after = i[dot + 1:].strip()
+            try:
+                l = int(dot_prev)
+                if not dot_after:
+                    break
+                v.append(dot_after)
+            except:
+                break
+    cache.clear()
+    cache.extend(v)
 
 
 read_cache()
 
 
 def writecache():
-    with open("./cache.txt", "w", encoding='utf-8') as f:
-        f.write('\n'.join(cache))
+    if not os.path.exists('cache'):
+        os.mkdirs('cache',exist_ok=1)
+    with open(f"./cache/{cache_name}.json", "w", encoding='utf-8') as f:
+        json.dump(cache, f, ensure_ascii=False)
 
 
 def sanitize(text):
@@ -90,21 +117,24 @@ class StructuredJSON(BaseModel):
     recipe_name: str
     ingredients: list[str]
 
+
 def translate_text(text, context: deque):
     global id
     if len(cache) > id:
         id += 1
         c = cache[id - 1]
-        if c:
+        if c and c != text:
             return c
     text = pre_clean(text)
     if text:
-        ret = ai_google(text, context) if ai_service=="google" else ai_openai(text,context)
+        ret = ai_google(text, context) if ai_service == "google" else ai_openai(
+            text, context)
     else:
-        ret=""
+        ret = ""
     if ret:
         cache.append(ret)
     return ret
+
 
 def ai_google(text, context):
     global id
@@ -135,8 +165,6 @@ def ai_google(text, context):
     return ret
 
 
-
-
 def ai_openai(text, context):
     global id
     messages = [
@@ -163,8 +191,8 @@ def translate_markdown(markdown_content):
     context = deque(maxlen=10)
     context.append(first)
     error = False
-    lines=markdown_content.splitlines()
-    prog=tqdm.tqdm(total=len(lines))
+    lines = markdown_content.split('\n\n').replace('\n', '$')
+    prog = tqdm.tqdm(total=len(lines))
     for line in lines:
         prog.update(1)
         match = pattern.match(line)
@@ -173,7 +201,8 @@ def translate_markdown(markdown_content):
             text = match.group(2)
             if not error:
                 for i in range(5):
-                    translated_text = translate_text(text, context)
+                    translated_text = translate_text(
+                        text.replace('$', '\n'), context)
                     if not translated_text:
                         waitfor()
                     else:
@@ -191,23 +220,29 @@ def translate_markdown(markdown_content):
         else:
             translated_content.append(line)
 
+    writecache()
     return '\n'.join(translated_content)
 
 
 def main():
     import sys
     input_file = sys.argv[1]
-    output_file = sys.argv[2] if len(sys.argv) > 2 else 'translation.md'
-
+    previous_file = sys.argv[2] if len(sys.argv) > 2 else None
+    global cache_name
+    cache_name=str(hash(input_file))
     with open(input_file, 'r', encoding='utf-8') as file:
         markdown_content = file.read()
+    if previous_file and os.path.exists(previous_file):
+        with open(previous_file, 'r', encoding='utf-8') as file:
+            c = file.read()
+            read_from_previous(c)
 
     translated_content = translate_markdown(markdown_content)
 
-    with open(output_file, 'w', encoding='utf-8') as file:
+    with open('translation.md', 'w', encoding='utf-8') as file:
         file.write(translated_content)
 
-    print(f"**Translated content has been saved to {output_file}")
+    print(f"**Translated content has been saved to {previous_file}")
 
 
 if __name__ == "__main__":
