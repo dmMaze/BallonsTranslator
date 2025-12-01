9c09395fad920bb4303c2ad0b83c609c432e8c21
cymon <cymon@litbig.com>
2025-12-01 13:59:01 +0900

    feat: Enhance Windows Launch Script for Conda Environment Activation
    - Conditionally activate "BallonsTranslator" Conda environment via conda activate
    - Implement fallback mechanism to search and execute alternative activate.bat if Conda not initialized

--- launch_win.bat
+++ launch_win.bat
@@ -1,11 +1,24 @@
 @REM dependencies\libraries\py310\python.exe F:\repos\BallonsTranslator\ballontranslator
 @REM @echo %PATH%
 
-cd %~dp0
-
 @echo off
+set ENV_NAME=BallonsTranslator
+cd /d "%~dp0"
 
-call conda activate bt 2>nul
+where conda >nul 2>nul
+if %errorlevel% equ 0 (
+    echo ### Attempting Conda environment activation: %ENV_NAME%
+    call conda activate %ENV_NAME%
+    goto execute
+)
+for %%D in ("%USERPROFILE%\anaconda3" "%USERPROFILE%\miniconda3" "C:\ProgramData\anaconda3" "C:\ProgramData\miniconda3") do (
+    if exist "%%D\Scripts\activate.bat" (
+        echo ### Conda not initialized. Searching for activate.bat to activate %ENV_NAME%.
+	call %%D\Scripts\activate.bat %ENV_NAME%
+	goto execute
+    )
+)
+:execute
 
 :: Set the path for PaddleOCR and PyTorch libraries
 set "PADDLE_PATH=%~dp0ballontrans_pylibs_win\Lib\site-packages\torch\lib"
