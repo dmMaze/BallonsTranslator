c144e389c7495778a1580f4184f4c5152764f436
tomaioo <hongsaygio.com@gmail.com>
2026-06-23 23:09:03 -0700

    fix(security): 2 improvements across 2 files
    - Security: Command Injection via os.system in update_translation.py
    - Security: Unsafe Deserialization in Papago Translator
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- scripts/update_translation.py
+++ scripts/update_translation.py
@@ -1,5 +1,6 @@
 import os
 import os.path as osp
+import subprocess
 from glob import glob
 
 from qtpy.QtCore import QLocale
@@ -10,10 +11,10 @@ if __name__ == '__main__':
     translate_dir = osp.join(program_dir, 'resources', 'translate')
     translate_path = osp.join(translate_dir, SYSLANG+'.ts')
 
-    cmd = 'pylupdate5 -verbose '+ \
-          ' '.join(glob(osp.join(program_dir, 'ballontranslator/ui/*.py'))) + \
-          ' -ts ' + translate_path
+    cmd = ['pylupdate5', '-verbose'] + \
+          glob(osp.join(program_dir, 'ballontranslator/ui/*.py')) + \
+          ['-ts', translate_path]
     
     print('target language: ', SYSLANG)
-    os.system(cmd)
+    subprocess.run(cmd)
     print(f'Saved to {translate_path}')
